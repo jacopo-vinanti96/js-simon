715db003c4ccafe4c7d0887af03f2658eeb8d808
Jacopo Vinanti <jacopovinanti@gmail.com>
2021-03-16 15:41:55 +0100

    w.i.p. #2

--- js/script.js
+++ js/script.js
@@ -10,6 +10,40 @@ for (var i = 0; i < 5; i++) {
 
 alert('Memorizza questi 5 numeri: ' + numbers);
 
-$(){
-  
-}
+var j = 5;
+countdown = setInterval(function () {
+  if ( j > 0 ) {
+    console.log(j);
+  } else {
+    console.log('tempo scaduto');
+    clearInterval(countdown);
+  }
+  j--;
+}, 1000);
+
+setTimeout( function () {
+
+  var guessCount = 0,
+      guessedNums = [],
+      userNum;
+
+  for (var i = 0; i < 5; i++) {
+    while ( isNaN(userNum) ) {
+      userNum = parseInt( prompt('Inserisci un ' + i + '° numero') );
+      if ( isNaN(userNum) ) {
+        alert( userNum + ' non è un valore numerico');
+      }
+    }
+    for (var i = 0; i < 5; i++) {
+      if ( userNum == numbers[i] ) {
+        guessCount += 1;
+        guessedNums.push(userNum);
+      }
+    }
+  }
+  if ( guessCount == 0) {
+    alert('Mi dispiace, non hai indovinato nessun numero');
+  } else {
+    alert('Hai indovinato ' + guessCount + ' numeri\nNumeri indovinati: ' + guessedNums )
+  }
+}, 5000);
